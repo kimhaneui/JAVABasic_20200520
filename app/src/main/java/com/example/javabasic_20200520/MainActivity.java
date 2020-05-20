1b8c144eeace06b9b8ef58256a6753320627cb9a
kimhaneui <kimhaneui2@naver.com>
2020-05-20 16:40:41 +0900

    binding 사용해보기

--- app/src/main/java/com/example/javabasic_20200520/MainActivity.java
+++ app/src/main/java/com/example/javabasic_20200520/MainActivity.java
@@ -1,23 +1,25 @@
 package com.example.javabasic_20200520;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
 
+import android.app.Activity;
 import android.os.Bundle;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import com.example.javabasic_20200520.databinding.ActivityMainBinding;
+
 public class MainActivity extends AppCompatActivity {
 
-    EditText contentEdt;
-    Button okBtn;
-    TextView contentTxt;
+
+    ActivityMainBinding binding;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        contentEdt =findViewById(R.id.contentEdt);
-        okBtn = findViewById(R.id.okBtn);
-        contentTxt = findViewById(R.id.contentTxt);
+
+        binding = DataBindingUtil.setContentView( this, R.layout.activity_main);
+
     }
 }
