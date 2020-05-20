0b883380578e27295a00fdd123cf7dd4c2a621f0
kimhaneui <kimhaneui2@naver.com>
2020-05-20 16:59:01 +0900

    이벤트 붙이는 다른 방법

--- app/src/main/java/com/example/javabasic_20200520/MainActivity.java
+++ app/src/main/java/com/example/javabasic_20200520/MainActivity.java
@@ -13,7 +13,7 @@ import android.widget.TextView;
 
 import com.example.javabasic_20200520.databinding.ActivityMainBinding;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
 
     ActivityMainBinding binding;
@@ -23,12 +23,19 @@ public class MainActivity extends AppCompatActivity {
 
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
 
-        binding.okBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Log.d("메인화면","확인버튼 클릭");
-            }
-        });
+//        binding.okBtn.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                Log.d("메인화면","확인버튼 클릭");
+//            }
+//        });
 
+        binding.okBtn.setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        Log.d("메인화면","자체 이벤트 코드");
     }
 }
